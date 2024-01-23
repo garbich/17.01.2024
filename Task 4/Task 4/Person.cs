0d941d19eafa3de694ae8b535edf0e20a0d7d466
garbich <131876643+garbich@users.noreply.github.com>
2024-01-23 23:09:43 +0200

    Add files via upload

---
+++ Task 4/Task 4/Person.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MySpace
+{
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ Task 4/Task 4/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace MySpace
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+
+           
+
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person("Artyr", 16));
+            persons.Add(new Person("Ostap", 36));
+            persons.Add(new Person("Nazar", 29));
+            persons.Add(new Person("Igor", 31));
+
+            List<Person> persons2 = persons.FindAll((person) => person.Age < 30);
+
+            persons2.ForEach((person) => Console.WriteLine(person.Name + " " + person.Age));
+
+        }
+
+    }
+}
