2a5db36bb99cc78c0364672c5acfbe9af6e2b52f
garbich <131876643+garbich@users.noreply.github.com>
2024-01-23 23:08:40 +0200

    Add files via upload

---
+++ Task 3/Task 3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace MySpace
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+
+            
+            List<string> list = new List<string>();
+
+            list.Add("ab");
+            list.Add("abc");
+            list.Add("abcde");
+            list.Add("abcd");
+
+            string result = list.OrderByDescending(x => x.Length).FirstOrDefault();
+
+            Console.WriteLine(result);
+
+
+
+
+        }
+
+    }
+}
