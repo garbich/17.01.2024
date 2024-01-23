570c6acb9841106cfc449e989e9a29449b897658
garbich <131876643+garbich@users.noreply.github.com>
2024-01-23 23:07:53 +0200

    Add files via upload

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace MySpace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+            int[] result = Array.FindAll(arr, (a) => a % 2 == 0);
+
+            Array.ForEach(result, (a) => Console.Write(a + " "));
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace MySpace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            List<int> list = new List<int>() { 1, 2, 3, 4 };
+
+            double result = (double)list.Average();
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
